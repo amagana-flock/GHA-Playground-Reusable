080fd11aaf629907e0cac5025fb715a4bccb990b
Andrew Magana <andrew.magana@flocksafety.com>
2023-03-28 14:19:06 -0400

    testing script

--- .github/scripts/commenter.js
+++ .github/scripts/commenter.js
@@ -1,17 +1,5 @@
-module.exports = async ({github, context, process}) => {
-    const {data: comments} = await github.rest.issues.listComments({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: context.issue.number,
-    });
-
-    const output = "Hello from GHA!";
-
-    github.rest.issues.createComment({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        body: output
-    });
-    
+module.exports = async function({ github, context, core, process }) {
+    const scriptPath = `.github/scripts/my-script.js`;
+    const script = require(`../${scriptPath}`);
+    await script({ github, context, core, process });
 }

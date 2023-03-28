b14de1c0a5a767698c4b4bc3d0242ad8068ac2c7
Andrew Magana <andrew.magana@flocksafety.com>
2023-03-28 14:23:39 -0400

    fix script

--- .github/scripts/commenter.js
+++ .github/scripts/commenter.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async function({ github, context, core, process }) {
-    const scriptPath = `.github/scripts/my-script.js`;
-    const script = require(`../${scriptPath}`);
+    const scriptPath = path.join(context.payload.repository.full_name, '.github/scripts/commenter.js');
+    const scriptContent = fs.readFileSync(scriptPath, 'utf8');
+    const script = eval(`(${scriptContent})`);
     await script({ github, context, core, process });
 }

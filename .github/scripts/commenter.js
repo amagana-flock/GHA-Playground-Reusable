142da78f2d9a1a0f28efa5b6be864e28e9989a9a
Andrew Magana <andrew.magana@flocksafety.com>
2023-03-31 15:54:14 -0400

    if else for output

--- .github/scripts/commenter.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports = async function({ github, context, core, process }) {
-    const scriptPath = path.join(context.payload.repository.full_name, '.github/scripts/commenter.js');
-    const scriptContent = fs.readFileSync(scriptPath, 'utf8');
-    const script = eval(`(${scriptContent})`);
-    await script({ github, context, core, process });
-}
